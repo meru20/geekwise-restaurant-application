085f7e56c7aa09316cef57961059a5abd679b26d
meru20 <merhawitgubsa@gmail.com>
2020-04-11 16:36:16 -0700

    Building the ordering page

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ function login() {
     }
     return false;
 }
+
+function order() {
+    var orderItem = document.getElementById('orderItem').value;
+    var orderQty = document.getElementById('orderQty').value;
+    var newOrder = {
+        orderItem: orderItem,
+        orderQty: orderQty
+    }
+
+    var orderCart = localStorage.getItem("orderCart");
+    if(!orderCart) {
+        localStorage.setItem("orderCart", JSON.stringify([newOrder]));
+    } else {
+        orderCartList = JSON.parse(orderCart);
+        newOrderCartList = [...orderCartList, newOrder];
+        localStorage.setItem("orderCart", JSON.stringify(newOrderCartList));
+    }
+    alert("Your " + orderQty + " " + orderItem + " have been ordered!")
+    return false;
+}
