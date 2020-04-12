3876fa6a88c2f1e0b47db9e05c5e2ca1d644d795
meru20 <merhawitgubsa@gmail.com>
2020-04-11 18:01:25 -0700

    Building the View Orders page

--- js/main.js
+++ js/main.js
@@ -12,19 +12,56 @@ function login() {
 function order() {
     var orderItem = document.getElementById('orderItem').value;
     var orderQty = document.getElementById('orderQty').value;
+    var orderCustomer = document.getElementById('orderCustomer').value;
     var newOrder = {
         orderItem: orderItem,
-        orderQty: orderQty
+        orderDate: new Date().toLocaleString(),
+        orderQty: orderQty,
+        orderCustomer: orderCustomer || "Unknown"
     }
 
     var orderCart = localStorage.getItem("orderCart");
     if(!orderCart) {
         localStorage.setItem("orderCart", JSON.stringify([newOrder]));
     } else {
-        orderCartList = JSON.parse(orderCart);
-        newOrderCartList = [...orderCartList, newOrder];
+        var orderCartList = JSON.parse(orderCart);
+        var newOrderCartList = [...orderCartList, newOrder];
         localStorage.setItem("orderCart", JSON.stringify(newOrderCartList));
     }
     alert("Your " + orderQty + " " + orderItem + " have been ordered!")
     return false;
+}
+
+function loadOrders() {
+    var orderCart = localStorage.getItem("orderCart");
+    var orderCartList = JSON.parse(orderCart);
+    var orderTable = "";
+
+    for(var i=0; i<orderCartList.length; i++) {
+        orderTable = orderTable + 
+            `<div class="row border-bottom border-top">
+                <div class="col-4 pt-4">
+                    <h2>${orderCartList[i].orderItem}</h2>
+                    <p>Ordered by: ${orderCartList[i].orderCustomer}</p>
+                </div>
+                <div class="col-4 text-center pt-5">
+                    <p>Ordered at: ${orderCartList[i].orderDate}</p>
+                    <p>Quantity: ${orderCartList[i].orderQty}</p>
+                </div>
+                <div class="col-4 text-center pt-5">
+                    <span class="mr-4"><button class="btn btn-success">Edit</button></span>
+                    <span class="mr-2"><button class="btn btn-danger" onclick="deleteItem(${i})">Delete</button></span>
+                </div>
+            </div>`
+    }
+
+    document.getElementById("orderTable").innerHTML = orderTable;
+}
+
+function deleteItem(index) {
+    var orderCart = localStorage.getItem("orderCart");
+    var orderCartList = JSON.parse(orderCart);
+    orderCartList.splice(index, 1);
+    localStorage.setItem("orderCart", JSON.stringify(orderCartList));
+    loadOrders();
 }
