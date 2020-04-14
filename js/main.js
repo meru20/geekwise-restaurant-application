a7815144f8421496f291dc5e4cbca01f11bb9e0e
meru20 <merhawitgubsa@gmail.com>
2020-04-13 21:34:39 -0700

    fixing small bug with window.location

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function login() {
     var user = document.getElementById('loginEmail').value;
     var password = document.getElementById('loginPassword').value;
     if (user == "mera@gmail.com" && password == "pass123") {
-        window.location.replace("order.html");
+        window.location = "order.html";
     } else {
         alert("Incorrect Username and/or Password");
     }
